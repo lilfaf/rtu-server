98dcb3f8309914d71aa9df4fba9a478b9a42e891
Louis Larpin <louis.larpin@gmail.com>
2017-04-06 17:13:40 +0200

    Disable origin check origin in user socket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Rtu.UserSocket do
   channel "track:*", Rtu.TrackChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000, check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
